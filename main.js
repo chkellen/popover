18c73a2dac463b6c782e04352c453ae4272b21d2
kraken <446073223@qq.com>
2018-09-19 00:28:37 +0800

    upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(clickMe).on('click', function() {
+    $(popover).show()
+    setTimeout(function() {
+      $(document).one('click', function() {
+        $(popover).hide()
+      })
+    }, 0)
+  
+  })
+  
+  
