cf1c7a08bea82e306d74940353fb469dca28129b
kraken <446073223@qq.com>
2018-09-21 16:55:42 +0800

    updata

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+
 $(clickMe).on('click', function() {
+  if(popover.style.display == 'block'){
+    $(popover).hide()
+  }else{
     $(popover).show()
+    console.log('show')
     setTimeout(function() {
       $(document).one('click', function() { 
+        console.log('我觉得这里不会执行')
+        console.log('hide')
         $(popover).hide()
       })
-    }, 0)   
-    $(popover).on('click',function(e){
-        e.stopPropagation()
-    })
+    }, 0)
+  }   
   })
+  $(popover).on('click',function(e){
+    e.stopPropagation()
+})
   
   
   
